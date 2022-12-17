bed1c931d6e5bbcbc5b34fe8352682074f3137f1
Ngô Văn Thiện <thiengo105@gmail.com>
2022-12-17 11:48:29 +0700

    update content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,6 +10,15 @@ const Footer = () => {
     <Wrapper>
       Made by{" "}
       <a
+        href="https://www.facebook.com/thanhlp18"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Thành
+      </a>{" "}
+      và
+      {" "}
+      <a
         href="https://www.facebook.com/thiengo105"
         target="_blank"
         rel="noopener noreferrer"
